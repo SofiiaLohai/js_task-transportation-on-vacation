f2108c71997d6c65537e649ebcf102d20ec926f8
Sofiia Lohai <logay1810@gmail.com>
2026-01-30 15:14:34 +0100

    Fixes after another review

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -7,13 +7,15 @@ function calculateRentalCost(days) {
   const costPerDay = 40;
   const discountFor7Days = 50;
   const discountFor3Days = 20;
+  const daysFor7DayDiscount = 7;
+  const daysFor3DayDiscount = 3;
   const totalPrice = days * costPerDay;
 
-  if (days >= 7) {
+  if (days >= daysFor7DayDiscount) {
     return totalPrice - discountFor7Days;
   }
 
-  if (days >= 3) {
+  if (days >= daysFor3DayDiscount) {
     return totalPrice - discountFor3Days;
   }
 
